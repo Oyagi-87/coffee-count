5be8a443a3f5f119c15374ffe52ded90e9718672
DESKTOP-S5CNSBF\USER <hnttsk0903@gmail.com>
2023-08-11 16:52:11 +0900

    アウトライン完成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [cups,setCups] = useState(0);
+  const [dripTime, setdripTime] = useState('00:00:00');
+  
+  function increaseCups(num){
+    const newTime = new Date();
+    setdripTime(newTime.toLocaleString());
+    setCups(cups + num);
+  }
+  
+  function decreaseCups(num){
+    setCups(cups - num);
+  }
+  
+  function clearCups(){
+    setdripTime(`ドリップされていません`);
+    setCups(0);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+    <h1>Coffee</h1>
+      <div>前回の抽出時刻</div>
+      <div>{dripTime}</div>
+      <p>残り{cups}杯</p>
+      <button onClick={function() { decreaseCups(1); }}>-1</button>
+      <button onClick={function() { decreaseCups(0.5); }}>-0.5</button>
+      <div></div>
+      <button onClick={function() { increaseCups(2); }}>+2</button>
+      <button onClick={function() { increaseCups(4); }}>+4</button>
+      <button onClick={function() { increaseCups(6); }}>+6</button>
+      <button onClick={function() { increaseCups(8); }}>+8</button>
+      <button onClick={function() { increaseCups(10); }}>+10</button>
+      <div></div>
+      <button onClick={function() { clearCups(); }}>clear</button>
     </div>
   );
 }
